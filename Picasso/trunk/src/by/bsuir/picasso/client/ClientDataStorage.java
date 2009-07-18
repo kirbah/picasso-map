25cc1e12cfd6d30a601b5d57601457bcc44632c6
Кирилл <kirbah@gmail.com>
2009-07-18 14:32:55 +0000

    Create separate classes for different UI parts

---
+++ Picasso/trunk/src/by/bsuir/picasso/client/ClientDataStorage.java
@@ -0,0 +1,34 @@
+package by.bsuir.picasso.client;
+
+import java.util.HashMap;
+
+import by.bsuir.picasso.shared.LoginInfo;
+import by.bsuir.picasso.shared.MarkerStorage;
+
+import com.google.gwt.maps.client.MapWidget;
+import com.google.gwt.maps.client.overlay.Marker;
+
+public class ClientDataStorage {
+  private LoginInfo loginInfo = null;
+
+  private HashMap<Marker, MarkerStorage> markersHash = new HashMap<Marker, MarkerStorage>();
+  private MarkerStorage addedMarker = null;
+  MapWidget map = null;
+
+  public LoginInfo getLoginInfo() {
+    return loginInfo;
+  }
+
+  public void setLoginInfo(LoginInfo loginInfo) {
+    this.loginInfo = loginInfo;
+  }
+
+  public MapWidget getMap() {
+    return map;
+  }
+
+  public void setMap(MapWidget map) {
+    this.map = map;
+  }
+
+}
