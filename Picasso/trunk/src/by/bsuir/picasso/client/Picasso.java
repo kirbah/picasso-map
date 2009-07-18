a6a0857037062e06913d6010526bb11ec8c4475e
Кирилл <kirbah@gmail.com>
2009-07-18 12:34:12 +0000

    Clean UI initial version

--- Picasso/trunk/src/by/bsuir/picasso/client/Picasso.java
+++ Picasso/trunk/src/by/bsuir/picasso/client/Picasso.java
@@ -17,151 +17,63 @@ import com.google.gwt.user.client.ui.RootPanel;
 
 public class Picasso implements EntryPoint {
 
-  /**
-   * This is the entry point method.
-   */
   public void onModuleLoad() {
-    
-    Menu menu = new Menu();   
-    
-    MenuItem item1 = new MenuItem("New");   
-    menu.add(item1);   
-       
-    MenuItem item2 = new MenuItem("Open File");   
-    menu.add(item2);   
-       
-    MenuBar bar = new MenuBar();   
-    bar.setBorders(true);   
-    bar.setStyleAttribute("borderTop", "none");   
-    bar.add(new MenuBarItem("File", menu));   
-       
-    Menu sub2 = new Menu();   
-    sub2.add(new MenuItem("Cut"));   
-    sub2.add(new MenuItem("Copy"));   
-       
-    MenuBarItem edit = new MenuBarItem("Edit", sub2);   
-    bar.add(edit);   
+    ContentPanel west = new ContentPanel();
+    west.setHeading("Markers");
+    ContentPanel center = new ContentPanel();
+    center.setHeading("Map");
+    ContentPanel south = new ContentPanel();
+    south.setHeading("Properties");
 
-    final Window complex = new Window();   
-    complex.setMaximizable(true);   
-    complex.setHeading("Map Window");   
-    complex.setWidth(200);   
-    complex.setHeight(350);
-    complex.setTopComponent(bar);
+    BorderLayoutData westData = new BorderLayoutData(LayoutRegion.WEST, 150);
+    westData.setSplit(true);
+    westData.setCollapsible(true);
+    westData.setMargins(new Margins(5));
 
-    HTML html = new HTML(
-        "This is HTML.  It will be interpreted as such if you specify "
-          + "the asHTML flag.", true);    
-    
-    complex.add(html);
+    BorderLayoutData centerData = new BorderLayoutData(LayoutRegion.CENTER);
+    centerData.setMargins(new Margins(5, 5, 5, 0));
 
-/*
-    ContentPanel panel = new ContentPanel();   
-    panel.setHeading("MenuBar Example");   
-    panel.setTopComponent(bar);
-    //panel.setSize("auto", "auto");
-    //panel.setAutoHeight(true);
-    //panel.setDeferHeight(true);
-    //panel.setBodyBorder(false);
-*/
-    
-//    RootPanel.get().add(complex);
-/*
-    final Window points = new Window();   
-    points.setMaximizable(true);   
-    points.setHeading("Points Window");   
-    points.setWidth(100);   
-    points.setHeight(500);
-//    RootPanel.get().add(points);
-  */  
-    
-    ContentPanel west = new ContentPanel();   
-    ContentPanel center = new ContentPanel();   
-    ContentPanel south = new ContentPanel();   
-    center.setHeading("BorderLayout Example");     
-    
-    BorderLayoutData westData = new BorderLayoutData(LayoutRegion.WEST, 150);   
-    westData.setSplit(true);   
-    westData.setCollapsible(true);   
-    westData.setMargins(new Margins(5));   
-  
-    BorderLayoutData centerData = new BorderLayoutData(LayoutRegion.CENTER);   
-    centerData.setMargins(new Margins(5, 5, 5, 0));   
-  
-    BorderLayoutData southData = new BorderLayoutData(LayoutRegion.SOUTH, 100);   
-    southData.setSplit(true);   
-    southData.setCollapsible(true);   
-    southData.setFloatable(true);   
-    southData.setMargins(new Margins(0, 5, 5, 5));    
-    
+    BorderLayoutData southData = new BorderLayoutData(LayoutRegion.SOUTH, 100);
+    southData.setSplit(true);
+    southData.setCollapsible(true);
+    southData.setFloatable(true);
+    southData.setMargins(new Margins(0, 5, 5, 5));
 
     ContentPanel panel = new ContentPanel();
-    //panel.setHeight(200);
-    //panel.setAutoHeight(true);
-    //panel.setDeferHeight(true);
-
-    
-    panel.setHeading("MenuBar Example");   
+    panel.setHeading("Picasso the world!");
+    MenuBar bar = buildMenuBar();
     panel.setTopComponent(bar);
     panel.setLayout(new BorderLayout());
-    panel.add(west, westData);   
-    panel.add(center, centerData);   
+    panel.add(west, westData);
+    panel.add(center, centerData);
     panel.add(south, southData);
 
     Viewport viewport = new Viewport();
-    viewport.setLayout(new BorderLayout());    
-    viewport.add(panel, new BorderLayoutData(LayoutRegion.CENTER));
-   // panel.layout();
-    
-    /*    
-    //viewport.setAutoHeight(true);
     viewport.setLayout(new BorderLayout());
-
-    viewport.add(west, westData);   
-    viewport.add(center, centerData);   
-    viewport.add(south, southData);
-*/    
+    viewport.add(panel, new BorderLayoutData(LayoutRegion.CENTER));
     RootPanel.get().add(viewport);
-//    viewport.show();
-
-//    complex.show();
-//    points.show();
-    
-//    RootPanel.get().add(panel);
-    
-/*
-    Window w = new Window();   
-    w.setLayout(new FlowLayout(10)); 
-    w.setHeading("Product Information");
-    w.setModal(true);
-    w.setSize(500, 300);
-    w.setActive(true);
-    w.setMaximizable(true);
-    w.setResizable(true);
-    //w.setEnabled(true);
-    w.setToolTip("The ExtGWT product page...");
-    //w.setUrl("http://www.extjs.com/products/gxt");
-    Button b = new Button("Нажми меня...");
-    w.add(b);
-    panel.add(w);
-    w.show();
-*/
-    
-    /*
-    Button b = new Button("Нажми меня...");
-    SelectionListener<ButtonEvent> sl;
-
-    sl = new SelectionListener<ButtonEvent>() {
-
-      @Override
-      public void componentSelected(ButtonEvent ce) {
-        MessageBox.alert("Нажата кнопка", "Вы нажали кнопку", null);
-      }
-
-    };
-    b.addSelectionListener(sl);
-    RootPanel.get().add(b);
-    */
   }
 
+  private MenuBar buildMenuBar() {
+    Menu menu = new Menu();
+
+    MenuItem item1 = new MenuItem("New");
+    menu.add(item1);
+
+    MenuItem item2 = new MenuItem("Open File");
+    menu.add(item2);
+
+    MenuBar bar = new MenuBar();
+    bar.setBorders(true);
+    bar.setStyleAttribute("borderTop", "none");
+    bar.add(new MenuBarItem("File", menu));
+
+    Menu sub2 = new Menu();
+    sub2.add(new MenuItem("Cut"));
+    sub2.add(new MenuItem("Copy"));
+
+    MenuBarItem edit = new MenuBarItem("Edit", sub2);
+    bar.add(edit);
+    return bar;
+  }
 }
