5d3d66b96779a7dd7dbbdfb1bb7183a710fa9f1f
Кирилл <@bf6e6540-b66d-11de-b71e-ab0a53e69959>
2009-07-18 14:32:55 +0000

    Create separate classes for different UI parts

---
+++ Picasso/trunk/src/by/bsuir/picasso/client/MenuHelper.java
@@ -0,0 +1,45 @@
+package by.bsuir.picasso.client;
+
+import com.extjs.gxt.ui.client.event.MenuEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.widget.menu.Menu;
+import com.extjs.gxt.ui.client.widget.menu.MenuBar;
+import com.extjs.gxt.ui.client.widget.menu.MenuBarItem;
+import com.extjs.gxt.ui.client.widget.menu.MenuItem;
+import com.extjs.gxt.ui.client.widget.menu.SeparatorMenuItem;
+
+public class MenuHelper {
+  public static MenuBar buildMenuBar(final ClientDataStorage cds) {
+    Menu menu = new Menu();
+
+    MenuItem item1 = new MenuItem("New");
+    menu.add(item1);
+
+    MenuItem item2 = new MenuItem("Open");
+    menu.add(item2);
+
+    menu.add(new SeparatorMenuItem());
+
+    MenuItem item3 = new MenuItem("Log out");
+    menu.add(item3);
+    item3.addSelectionListener(new SelectionListener<MenuEvent>() {
+      public void componentSelected(MenuEvent ce) {
+        String logoutURL = cds.getLoginInfo().getLogoutUrl();
+        com.google.gwt.user.client.Window.Location.replace(logoutURL);
+      }
+    });
+
+    MenuBar bar = new MenuBar();
+    bar.setBorders(true);
+    bar.setStyleAttribute("borderTop", "none");
+    bar.add(new MenuBarItem("File", menu));
+
+    Menu sub2 = new Menu();
+    sub2.add(new MenuItem("Cut"));
+    sub2.add(new MenuItem("Copy"));
+
+    MenuBarItem edit = new MenuBarItem("Edit", sub2);
+    bar.add(edit);
+    return bar;
+  }
+}
--- Picasso/trunk/src/by/bsuir/picasso/client/Picasso.java
+++ Picasso/trunk/src/by/bsuir/picasso/client/Picasso.java
@@ -1,54 +1,31 @@
 package by.bsuir.picasso.client;
 
-import java.util.HashMap;
-
 import by.bsuir.picasso.client.service.LoginService;
 import by.bsuir.picasso.client.service.LoginServiceAsync;
 import by.bsuir.picasso.shared.LoginInfo;
-import by.bsuir.picasso.shared.MarkerStorage;
 
 import com.extjs.gxt.ui.client.Style.LayoutRegion;
-import com.extjs.gxt.ui.client.event.ButtonEvent;
-import com.extjs.gxt.ui.client.event.Events;
-import com.extjs.gxt.ui.client.event.MenuEvent;
-import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
-import com.extjs.gxt.ui.client.widget.HtmlContainer;
 import com.extjs.gxt.ui.client.widget.Viewport;
-import com.extjs.gxt.ui.client.widget.Window;
-import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
-import com.extjs.gxt.ui.client.widget.menu.Menu;
 import com.extjs.gxt.ui.client.widget.menu.MenuBar;
-import com.extjs.gxt.ui.client.widget.menu.MenuBarItem;
-import com.extjs.gxt.ui.client.widget.menu.MenuItem;
-import com.extjs.gxt.ui.client.widget.menu.SeparatorMenuItem;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.maps.client.MapType;
-import com.google.gwt.maps.client.MapWidget;
-import com.google.gwt.maps.client.Maps;
-import com.google.gwt.maps.client.geom.LatLng;
-import com.google.gwt.maps.client.overlay.Marker;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Picasso implements EntryPoint {
-  private LoginInfo loginInfo = null;
-
-  private HashMap<Marker, MarkerStorage> markersHash = new HashMap<Marker, MarkerStorage>();
-  private MarkerStorage addedMarker = null;
-  MapWidget map = null;
+  private ClientDataStorage cds = new ClientDataStorage();
 
   public void onModuleLoad() {
     /*
-      if (!Maps.isLoaded()) { com.google.gwt.user.client.Window.alert("The Maps API is not installed."
+      if (!Maps.isLoaded()) {
+       com.google.gwt.user.client.Window.alert("The Maps API is not installed."
        + " The <script> tag that loads the Maps API may be missing or your Maps key may be wrong."); return; }
       
       if (!Maps.isBrowserCompatible()) {
@@ -63,8 +40,8 @@ public class Picasso implements EntryPoint {
       }
 
       public void onSuccess(LoginInfo result) {
-        loginInfo = result;
-        if(loginInfo.isLoggedIn()) {
+        cds.setLoginInfo(result);
+        if(result.isLoggedIn()) {
           loadApplicationUI();
         } else {
           loadLoginUI();
@@ -78,7 +55,7 @@ public class Picasso implements EntryPoint {
     VerticalPanel loginPanel = new VerticalPanel();
     Label loginLabel = new Label("Please sign in to your Google Account to access the application.");
     Anchor signInLink = new Anchor("Sign In");
-    signInLink.setHref(loginInfo.getLoginUrl());
+    signInLink.setHref(cds.getLoginInfo().getLoginUrl());
     loginPanel.add(loginLabel);
     loginPanel.add(signInLink);
     RootPanel.get().add(loginPanel);
@@ -107,7 +84,7 @@ public class Picasso implements EntryPoint {
 
     ContentPanel panel = new ContentPanel();
     panel.setHeading("Picasso the world!");
-    MenuBar bar = buildMenuBar();
+    MenuBar bar = MenuHelper.buildMenuBar(cds);
     panel.setTopComponent(bar);
     panel.setLayout(new BorderLayout());
     panel.add(west, westData);
@@ -133,37 +110,4 @@ public class Picasso implements EntryPoint {
     return center;
   }
 
-  private MenuBar buildMenuBar() {
-    Menu menu = new Menu();
-
-    MenuItem item1 = new MenuItem("New");
-    menu.add(item1);
-
-    MenuItem item2 = new MenuItem("Open");
-    menu.add(item2);
-
-    menu.add(new SeparatorMenuItem());
-
-    MenuItem item3 = new MenuItem("Log out");
-    menu.add(item3);
-    item3.addSelectionListener(new SelectionListener<MenuEvent>() {
-      public void componentSelected(MenuEvent ce) {
-        String logoutURL = loginInfo.getLogoutUrl();
-        com.google.gwt.user.client.Window.Location.replace(logoutURL);
-      }
-    });
-
-    MenuBar bar = new MenuBar();
-    bar.setBorders(true);
-    bar.setStyleAttribute("borderTop", "none");
-    bar.add(new MenuBarItem("File", menu));
-
-    Menu sub2 = new Menu();
-    sub2.add(new MenuItem("Cut"));
-    sub2.add(new MenuItem("Copy"));
-
-    MenuBarItem edit = new MenuBarItem("Edit", sub2);
-    bar.add(edit);
-    return bar;
-  }
 }
