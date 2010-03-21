72a9b0822850bda166a3271fd8a5cd19cb192b3e
kirbah@gmail.com <kirbah@gmail.com@bf6e6540-b66d-11de-b71e-ab0a53e69959>
2010-03-21 20:42:46 +0000

    Get elevation data from ws.geonames.org

--- Picasso/trunk/src/by/bsuir/picasso/client/ElevationExport.java
+++ Picasso/trunk/src/by/bsuir/picasso/client/ElevationExport.java
@@ -1,51 +1,133 @@
 package by.bsuir.picasso.client;
 
-import by.bsuir.picasso.client.data.ClientDataStorage;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
-import com.extjs.gxt.ui.client.widget.Window;
-import com.extjs.gxt.ui.client.widget.button.Button;
-import com.extjs.gxt.ui.client.widget.form.FormButtonBinding;
-import com.extjs.gxt.ui.client.widget.form.FormPanel;
 import com.extjs.gxt.ui.client.widget.form.TextArea;
-import com.extjs.gxt.ui.client.widget.layout.FitLayout;
-import com.extjs.gxt.ui.client.widget.layout.FormData;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
 
 public class ElevationExport {
-  public static Window showElevationWindow(final ClientDataStorage cds) {
-    FormData formData = new FormData("-20");
-    final Window window = new Window();
-    window.setSize(300, 200);
-    window.setPlain(true);
-    window.setModal(true);
-    window.setBlinkModal(true);
-    window.setHeading(Picasso.CONSTANTS.elevationExport());
-    window.setLayout(new FitLayout());
+  private static final int POINTS_PER_REQUEST = 3;
+  private static final String WS_URL = "http://ws.geonames.org/";
 
-    FormPanel panel = new FormPanel();  
-    panel.setPadding(0);  
-    panel.setFrame(false);  
-    panel.setHeaderVisible(false);  
-    panel.setBodyBorder(false);  
-    panel.setButtonAlign(HorizontalAlignment.CENTER);
-    //panel.setLayout(new FitLayout());
-    
-    TextArea description = new TextArea();  
-    //description.setPreventScrollbars(true);  
-    description.setFieldLabel("Description");  
-    panel.add(description, formData);
-    
-    
-    Button b = new Button("Submit");  
-    panel.addButton(b);  
-    panel.addButton(new Button("Cancel"));  
+  private static TextArea theDesription;
+  private static double theLat = 0.0;
+  private static double theLng = 0.0;
+  private static double theLngStart = 0.0;
+  private static double theLatEnd = 0.0;
+  private static double theLngEnd = 0.0;
+  private static double theLatStep = 0.0;
+  private static double theLngStep = 0.0;
+  private static List<Double> _pointsLat;
+  private static List<Double> _pointsLng;
 
-    FormButtonBinding binding = new FormButtonBinding(panel);  
-    binding.addButton(b); 
+  public static void begin(TextArea description, double latStart, double lngStart, double latEnd, double lngEnd,
+      double latStep, double lngStep) {
+    theDesription = description;
+    theLat = latStart;
+    theLng = lngStart;
+    theLngStart = lngStart;
+    theLatEnd = latEnd;
+    theLngEnd = lngEnd;
+    theLatStep = latStep;
+    theLngStep = lngStep;
 
-    window.add(panel);
-    window.show();
-
-    return window;
+    nextStep();
   }
+
+  private static void nextStep() {
+    _pointsLat = new ArrayList<Double>();
+    _pointsLng = new ArrayList<Double>();
+    int i = 0;
+    for (; theLat <= theLatEnd; theLat = round(theLat + theLatStep)) {
+      for (; theLng <= theLngEnd; theLng = round(theLng + theLngStep)) {
+        _pointsLat.add(theLat);
+        _pointsLng.add(theLng);
+        i++;
+        if (i == POINTS_PER_REQUEST) {
+          theLng = round(theLng + theLngStep);
+          break;
+        }
+      }
+      if (i == POINTS_PER_REQUEST) {
+        break;
+      }
+      theLng = theLngStart;
+    }
+    if (_pointsLat.size() > 0) {
+      fetchData();
+    }
+  }
+
+  private static double round(double in) {
+    int rounded = (int) (in * 10000.0);
+    return ((double) rounded) / 10000.0;
+  }
+
+  private static void fetchData() {
+    String url = WS_URL + "astergdem?lats"; // =53.93,53.93,53.92&lngs=27.69,27.70,27.70
+    boolean isFirst = true;
+    for (Double lat : _pointsLat) {
+      if (isFirst) {
+        url += "=";
+        isFirst = false;
+      } else {
+        url += ",";
+      }
+      url += lat;
+    }
+    url += "&lngs";
+    isFirst = true;
+    for (Double lng : _pointsLng) {
+      if (isFirst) {
+        url += "=";
+        isFirst = false;
+      } else {
+        url += ",";
+      }
+      url += lng;
+    }
+    RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, URL.encode(url));
+
+    try {
+      Request request = builder.sendRequest(null, new RequestCallback() {
+        public void onError(Request request, Throwable exception) {
+          // Couldn't connect to server (could be timeout, SOP violation, etc.)
+          com.google.gwt.user.client.Window.alert("Couldn't connect to WebService: " + exception.getMessage());
+        }
+
+        public void onResponseReceived(Request request, Response response) {
+          if (200 == response.getStatusCode()) {
+            // Process the response in response.getText()
+            String result = response.getText();
+            // com.google.gwt.user.client.Window.alert(result);
+            String[] resMeters = result.split("\r\n");
+            String old = theDesription.getRawValue();
+            String nvalue = "";
+            int i = 0;
+            for (String pointMeters : resMeters) {
+              nvalue += _pointsLat.get(i) + ";" + _pointsLng.get(i) + ";" + pointMeters + "\r\n";
+              i++;
+            }
+            theDesription.setRawValue(old + nvalue);
+
+            nextStep();
+          } else {
+            // Handle the error. Can get the status text from
+            // response.getStatusText()
+            com.google.gwt.user.client.Window.alert("Error: " + response.getStatusText());
+          }
+        }
+      });
+    } catch (RequestException e) {
+      // Couldn't connect to server
+      com.google.gwt.user.client.Window.alert("WebService connection error: " + e.getMessage());
+    }
+  }
+
 }
