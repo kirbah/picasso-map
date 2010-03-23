8de8d0ec3c3b985a310a4379147efc54392172a2
kirbah@gmail.com <kirbah@gmail.com@bf6e6540-b66d-11de-b71e-ab0a53e69959>
2010-03-23 18:22:56 +0000

    Get min/max for lat/lng from map and use for export boundaries calculation

--- Picasso/trunk/src/by/bsuir/picasso/client/ElevationExportWindow.java
+++ Picasso/trunk/src/by/bsuir/picasso/client/ElevationExportWindow.java
@@ -1,9 +1,14 @@
 package by.bsuir.picasso.client;
 
 import by.bsuir.picasso.client.data.ClientDataStorage;
+import by.bsuir.picasso.client.data.MarkerModel;
+import by.bsuir.picasso.client.data.PolyModel;
 
 import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.Events;
+import com.extjs.gxt.ui.client.event.FieldEvent;
+import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.Window;
@@ -19,8 +24,18 @@ import com.extjs.gxt.ui.client.widget.layout.ColumnLayout;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.layout.FormData;
 import com.extjs.gxt.ui.client.widget.layout.FormLayout;
+import com.google.gwt.maps.client.geom.LatLng;
+import com.google.gwt.maps.client.geom.LatLngBounds;
 
 public class ElevationExportWindow {
+  private static double _latMin;
+  private static double _latMax;
+  private static double _lngMin;
+  private static double _lngMax;
+
+  private static double _latStep;
+  private static double _lngStep;
+
   public static Window showElevationWindow(final ClientDataStorage cds) {
     FormData formData = new FormData("100%");
     final Window window = new Window();
@@ -49,22 +64,32 @@ public class ElevationExportWindow {
     layout.setLabelWidth(75);
     left.setLayout(layout);
 
-    NumberField latMin = new NumberField();
-    latMin.setFieldLabel("Min");
-    left.add(latMin, formData);
+    final NumberField latMaxField = new NumberField();
+    latMaxField.setFieldLabel("Max");
+    left.add(latMaxField, formData);
 
-    NumberField latMax = new NumberField();
-    latMax.setFieldLabel("Max");
-    left.add(latMax, formData);
+    final NumberField latMinField = new NumberField();
+    latMinField.setFieldLabel("Min");
+    left.add(latMinField, formData);
 
-    NumberField latSteps = new NumberField();
+    final NumberField latSteps = new NumberField();
     latSteps.setFieldLabel("Pieces");
+    latSteps.setPropertyEditorType(Integer.class);
     latSteps.setValue(20);
     left.add(latSteps, formData);
 
-    LabelField latDelta = new LabelField();
+    final LabelField latDelta = new LabelField();
     latDelta.setFieldLabel("Delta");
     left.add(latDelta, formData);
+
+    Listener<FieldEvent> latitudeCalcListener = new Listener<FieldEvent>() {
+      public void handleEvent(FieldEvent be) {
+        latitudeCalculate(latMaxField, latMinField, latSteps, latDelta);
+      }
+    };
+    latMinField.addListener(Events.Change, latitudeCalcListener);
+    latMaxField.addListener(Events.Change, latitudeCalcListener);
+    latSteps.addListener(Events.Change, latitudeCalcListener);
 
     // Longitude
     FieldSet right = new FieldSet();
@@ -74,29 +99,37 @@ public class ElevationExportWindow {
     layout.setLabelWidth(75);
     right.setLayout(layout);
 
-    NumberField lngMin = new NumberField();
-    lngMin.setFieldLabel("Min");
-    right.add(lngMin, formData);
+    final NumberField lngMaxField = new NumberField();
+    lngMaxField.setFieldLabel("Max");
+    right.add(lngMaxField, formData);
 
-    NumberField lngMax = new NumberField();
-    lngMax.setFieldLabel("Max");
-    right.add(lngMax, formData);
+    final NumberField lngMinField = new NumberField();
+    lngMinField.setFieldLabel("Min");
+    right.add(lngMinField, formData);
 
-    NumberField lngSteps = new NumberField();
+    final NumberField lngSteps = new NumberField();
     lngSteps.setFieldLabel("Pieces");
     lngSteps.setValue(20);
     right.add(lngSteps, formData);
 
-    LabelField lngDelta = new LabelField();
+    final LabelField lngDelta = new LabelField();
     lngDelta.setFieldLabel("Delta");
     right.add(lngDelta, formData);
+
+    Listener<FieldEvent> longitudeCalcListener = new Listener<FieldEvent>() {
+      public void handleEvent(FieldEvent be) {
+        longitudeCalculate(lngMaxField, lngMinField, lngSteps, lngDelta);
+      }
+    };
+    lngMinField.addListener(Events.Change, longitudeCalcListener);
+    lngMaxField.addListener(Events.Change, longitudeCalcListener);
+    lngSteps.addListener(Events.Change, longitudeCalcListener);
 
     main.add(left, new ColumnData(.5));
     main.add(right, new ColumnData(.5));
     panel.add(main, new FormData("100%"));
 
     final TextArea description = new TextArea();
-    // description.setPreventScrollbars(true);
     description.setFieldLabel("Description");
     panel.add(description, formData);
 
@@ -105,7 +138,11 @@ public class ElevationExportWindow {
     submit.addSelectionListener(new SelectionListener<ButtonEvent>() {
       public void componentSelected(ButtonEvent ce) {
         description.setRawValue("LAT;LNG;METERS\r\n");
-        ElevationExport.begin(description, 53.93, 27.69, 53.95, 27.75, 0.01, 0.01);
+        double latMin = latMinField.getValue().doubleValue();
+        double latMax = latMaxField.getValue().doubleValue();
+        double lngMin = lngMinField.getValue().doubleValue();
+        double lngMax = lngMaxField.getValue().doubleValue();
+        ElevationExport.begin(description, latMin, lngMin, latMax, lngMax, _latStep, _lngStep);
       }
     });
 
@@ -118,6 +155,83 @@ public class ElevationExportWindow {
     window.add(panel);
     window.show();
 
+    // Visible map
+    LatLngBounds mapBounds = cds.getMap().getBounds();
+    _latMax = mapBounds.getNorthEast().getLatitude();
+    _lngMax = mapBounds.getNorthEast().getLongitude();
+    _latMin = mapBounds.getSouthWest().getLatitude();
+    _lngMin = mapBounds.getSouthWest().getLongitude();
+
+    // Markers
+    for (MarkerModel markerModel : cds.getMarkersStore().getModels()) {
+      LatLng point = markerModel.getMarker().getLatLng();
+      if (_latMin > point.getLatitude()) {
+        _latMin = point.getLatitude();
+      }
+      if (_latMax < point.getLatitude()) {
+        _latMax = point.getLatitude();
+      }
+      if (_lngMin > point.getLongitude()) {
+        _lngMin = point.getLongitude();
+      }
+      if (_lngMax < point.getLongitude()) {
+        _lngMax = point.getLongitude();
+      }
+    }
+
+    // Polygons
+    for (PolyModel polyModel : cds.getPolygonStore().getModels()) {
+      LatLngBounds bounds = polyModel.getPolygon().getBounds();
+      if (_latMax < bounds.getNorthEast().getLatitude()) {
+        _latMax = bounds.getNorthEast().getLatitude();
+      }
+      if (_lngMax < bounds.getNorthEast().getLongitude()) {
+        _lngMax = bounds.getNorthEast().getLongitude();
+      }
+      if (_latMin > bounds.getSouthWest().getLatitude()) {
+        _latMin = bounds.getSouthWest().getLatitude();
+      }
+      if (_lngMin > bounds.getSouthWest().getLongitude()) {
+        _lngMin = bounds.getSouthWest().getLongitude();
+      }
+    }
+
+    // Set min/max fields values
+    latMinField.setValue(round(_latMin - 0.01));
+    latMaxField.setValue(round(_latMax + 0.01));
+    lngMinField.setValue(round(_lngMin - 0.01));
+    lngMaxField.setValue(round(_lngMax + 0.01));
+
+    latitudeCalculate(latMaxField, latMinField, latSteps, latDelta);
+    longitudeCalculate(lngMaxField, lngMinField, lngSteps, lngDelta);
+
     return window;
+  }
+
+  private static double round(double in) {
+    int rounded = (int) (in * 100.0);
+    return ((double) rounded) / 100.0;
+  }
+
+  private static void latitudeCalculate(final NumberField latMaxField, final NumberField latMinField,
+      final NumberField latSteps, final LabelField latDelta) {
+    double delta = 0.1;
+    if (latSteps.getValue().intValue() > 0) {
+      delta = (latMaxField.getValue().doubleValue() - latMinField.getValue().doubleValue())
+          / latSteps.getValue().doubleValue();
+    }
+    _latStep = ElevationExport.round(delta);
+    latDelta.setValue(_latStep);
+  }
+
+  private static void longitudeCalculate(final NumberField lngMaxField, final NumberField lngMinField,
+      final NumberField lngSteps, final LabelField lngDelta) {
+    double delta = 0.1;
+    if (lngSteps.getValue().intValue() > 0) {
+      delta = (lngMaxField.getValue().doubleValue() - lngMinField.getValue().doubleValue())
+          / lngSteps.getValue().doubleValue();
+    }
+    _lngStep = ElevationExport.round(delta);
+    lngDelta.setValue(_lngStep);
   }
 }
